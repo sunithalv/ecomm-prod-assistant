3baa9547c7cbc1391c2b790bf4d6d24850a5252d
sunithalv <sunithalv05@gmail.com>
2025-09-11 20:38:06 +0400

    latest changes

--- prod_assistant/etl/data_ingestion.py
+++ prod_assistant/etl/data_ingestion.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 from typing import List
 from langchain_core.documents import Document
 from langchain_astradb import AstraDBVectorStore
-from prod_assistant.utils.model_loader import ModelLoader
-from prod_assistant.utils.config_loader import load_config
+from utils.model_loader import ModelLoader
+from utils.config_loader import load_config
 
 class DataIngestion:
     """
